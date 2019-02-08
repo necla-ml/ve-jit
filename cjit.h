3204cccb3ac2a2d6134efba5edbaa4d7342be06b
kruus <kruus@nec-labs.com>
2019-02-09 08:10:34 +0900

    tracking dlopen bugs:
    Bug 1 (reported) dlopen of .so produced by nc++ with C++ -fPIC objects
          causes illegal instruction during cleanup code
    Bug 2 DllBuild within libjit1.so fails [but in libjit1.a succeeds]
          when using a clang C-intrinsics JIT dll.
       (There is 1 successful way to JIT clang intrinsics code, for now)

---
+++ cjit.h
@@ -0,0 +1,23 @@
+#ifndef CJIT_H
+#define CJIT_H
+
+#ifdef __cplusplus
+extern "C" {
+//#warning "Good, jitpage.h is extern 'C' for C++ compile"
+#endif
+    /// \group misc C utilities
+    /// accumulate them here, for now
+    //@{
+    /** try \c dlopen(relpath,opt), and if that fails, try \c realpath(relpath).
+     * \return NULL on error */
+    void * dlopen_rel(char const* const relpath, int opt);
+
+    /** debug dump of a .so library, from dlopen[_rel] */
+    void dl_dump(void const* const dlopen_handle);
+    //@}
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+// vim: ts=4 sw=4 et cindent cino=^=l0,\:.5s,=-.5s,N-s,g.5s,b1 cinkeys=0{,0},0),\:,0#,!^F,o,O,e,0=break
+#endif // CJIT_H
