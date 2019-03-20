3b3fb82fe6410d802817b93aff18767a6a1ecd63
Erik Kruus <kruus@nec-labs.com>
2019-03-20 15:40:22 -0400

    add the 21-bit VE version of fast divisibility test, adapting https://arxiv.org/pdf/1902.01961.pdf

--- intutil.h
+++ intutil.h
@@ -43,15 +43,54 @@ FASTDIV_INLINE uint64_t computeM_uB(uint32_t d) {
 FASTDIV_INLINE uint32_t fastdiv_uB(uint32_t const a, uint64_t const M) {
     return M*a >> FASTDIV_C; // 2 ops: mul, shr
 }
-/** fastmod_uB computes (a % d) given precomputed M.
+/** fastmod_uB computes \c (a%d) given precomputed \c M.
+ * \c M is a pre-computed function of \c d, from \c computeM_uB(d)
  * Probably nicer to calc. uint64_t D=a/d using fastdiv_uB, and then R=a-D*d.
  * (retain "everythin" in u64 registers).
- * \pre a,d < (1<<21). */
+ * \pre \c a and \c d < (1<<21).
+ * When d is a power of \f$(v&(v-1)) == 0\f$, just use shifts, \ref positivePow2.
+ */
 FASTDIV_INLINE uint32_t fastmod_uB(uint32_t const a, uint64_t const M, uint32_t const d) {
+    // Lemire original
+    //uint64_t lowbits = M * a;
+    //return ((__uint128_t)lowbits * d) >> 64;
+    // lowbits version
+#if 0
     // ugly with 'lowbits' approach, nicer to just do the fastdiv followed by mul and sub :(
-    // 3-4 ops: mul, shr, mul-sub... AND get a/d too (i.e. divmod adds 1 op to the division)
+    // HOWEVER, in JIT code VE has the mask as an immediate constant!
+    uint64_t lowbits = (M * a) & FASTDIV_CMASK;
+    return (lowbits * d) >> FASTDIV_C; // ops: mul, and, mul, shr [4 ops]
+    // divmod would be like:
+    //    i = M*a;
+    //    div = i>>C;
+    //    j = i & C_MASK; // in parellel w/ prev op
+    //    k = j * d;
+    //    mod = k>>C;
+#endif
+    // 3-4 ops: mul, shr, mul-sub... AND get a/d too
+    // (VE has no fused mul-sub for unsigned, though, so 4 ops
     return a - (M*a>>FASTDIV_C)*d;
+    // divmod would be like:
+    //    i = M*a;
+    //    div = i>>C;
+    //    j = div *d;
+    //    mod = a - j;
+    // so fewer ops, still 2 multiplies
 }
+
+/** I don't need it, but for completeness, there is a fast divisibility check.
+ * This is pretty nice for VE because it avoids rotation is fairly expensive
+ * on VE (no single-instruction rotate op).
+ * \pre \c a and \c d < (1<<21).
+ * For power-of-two, you should use (a&(a-1)==0)
+ */
+FASTDIV_INLINE int fastdivisible_uB(uint32_t const a, uint64_t const M) {
+    return ((M*a) & FASTDIV_CMASK) <= M-1;
+    // obviously much faster than the naive way using fastmod:
+    // return (a - ((M*a)>>C)*d) == 0;
+    //         ----fastmod_uB----
+}
+
 #undef FASTDIV_CMAKE
 //#undef FASTDIV_C
 //#undef FASTDIV_SAFEMAX
